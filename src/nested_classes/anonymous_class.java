00b5e9936d306887b3fa51e2d987d465e71cf3f3
PolinaSuvorova <polina.tatara@gmail.com>
2024-03-11 17:08:29 +0300

    Example of Nested Classes anonymous class

---
+++ src/nested_classes/anonymous_class.java
@@ -0,0 +1,18 @@
+package nested_classes;
+
+import java.util.Arrays;
+
+public class anonymous_class {
+    public static void main(String[] args) {
+        Math2 math2 = new Math2(){
+            @Override
+            public int doOperation(int a, int b) {
+            return a+b;}
+        };
+        System.out.println(  math2.doOperation(1,2) );
+    }
+}
+
+interface Math2{
+   int  doOperation(int a, int b);
+}
